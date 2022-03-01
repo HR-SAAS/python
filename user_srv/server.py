2e026744ec2f738a6d44c82c961c55102df7007e
blankqwq <1136589038@qq.com>
2022-03-01 10:18:06 +0800

    add arguments

--- user_srv/server.py
+++ user_srv/server.py
@@ -1,18 +1,38 @@
+import argparse
+import os.path
+import signal
+import sys
 from concurrent import futures
 
 import grpc
 
-from user_srv.proto import user_pb2,user_pb2_grpc
+# 配置引入路径
+BASEDIR = os.path.dirname(os.path.abspath(os.path.dirname(__file__)))
+sys.path.insert(0, BASEDIR)
+from user_srv.proto import user_pb2, user_pb2_grpc
 from user_srv.handler.user import UserService
 from loguru import logger
+
+
+def on_exit(signum, frame):
+    logger.warning('进程中断')
+    sys.exit(0)
+
 
 if __name__ == '__main__':
     logger.add("logs/user_srv_{time}.log")
 
-
+    argument = argparse.ArgumentParser()
+    argument.add_argument('--port',nargs='?',type=int,default=5002,help='listen port')
+    argument.add_argument('--host',nargs='?',type=str,default='127.0.0.1',help='listen host')
+    args = argument.parse_args()
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-    user_pb2_grpc.add_UserServicer_to_server(UserService(),server)
-    server.add_insecure_port("[::]:5002")
-    logger.info(f"服务已经启动 localhost:5002")
+    user_pb2_grpc.add_UserServicer_to_server(UserService(), server)
+    server.add_insecure_port(f"{args.host}:{args.port}")
+
+    signal.signal(signal.SIGINT, on_exit)
+    signal.signal(signal.SIGTERM, on_exit)
+
+    logger.info(f"服务已经启动 {args.host}:{args.port}")
     server.start()
     server.wait_for_termination()
