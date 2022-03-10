4fca4e1be1845315a116cf1743020faddb3a44aa
blankqwq <1136589038@qq.com>
2022-03-10 21:47:20 +0800

    start consul

---
+++ common/health_check/handler/health.py
@@ -0,0 +1,13 @@
+import grpc
+
+from user_srv.proto import health_pb2,health_pb2_grpc
+
+
+class HealthService(health_pb2_grpc.HealthServicer):
+    def Check(self, request, context):
+        """Missing associated documentation comment in .proto file."""
+        return health_pb2.HealthCheckResponse(status=1)
+
+    def Watch(self, request, context):
+        """Missing associated documentation comment in .proto file."""
+        return health_pb2.HealthCheckResponse(status=1)
