ae7d1e88c0f4ead043cc99cd0974f3103ad93681
blankqwq <1136589038@qq.com>
2022-03-20 11:54:54 +0800

    更新proto

--- company_srv/proto/company_pb2.py
+++ company_srv/proto/company_pb2.py
@@ -15,10 +15,12 @@ _sym_db = _symbol_database.Default()
 from google.protobuf import empty_pb2 as google_dot_protobuf_dot_empty__pb2
 
 
-DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\rcompany.proto\x1a\x1bgoogle/protobuf/empty.proto\"%\n\x17GetCompanyDetailRequest\x12\n\n\x02id\x18\x01 \x01(\x03\"D\n\x15GetCompanyListRequest\x12\x0e\n\x06search\x18\x01 \x01(\t\x12\x0c\n\x04page\x18\x02 \x01(\x05\x12\r\n\x05limit\x18\x03 \x01(\x05\"D\n\x13\x43ompanyListResponse\x12\x1e\n\x04\x64\x61ta\x18\x01 \x03(\x0b\x32\x10.CompanyResponse\x12\r\n\x05total\x18\x02 \x01(\x05\"\"\n\x14\x44\x65leteCompanyRequest\x12\n\n\x02id\x18\x01 \x01(\x03\"\xc3\x01\n\x14UpdateCompanyRequest\x12\n\n\x02id\x18\x01 \x01(\x03\x12\x0c\n\x04name\x18\x02 \x01(\t\x12\x0c\n\x04\x64\x65sc\x18\x03 \x01(\t\x12\x0f\n\x07website\x18\x04 \x01(\t\x12\x0e\n\x06\x63onfig\x18\x05 \x01(\t\x12\x0c\n\x04tags\x18\x06 \x01(\t\x12\x0f\n\x07\x61\x64\x64ress\x18\x07 \x01(\t\x12\x0c\n\x04info\x18\x08 \x01(\t\x12\x12\n\ncreator_id\x18\t \x01(\x03\x12\x11\n\tparent_id\x18\n \x01(\x03\x12\x0e\n\x06status\x18\x0b \x01(\x05\"\xbe\x01\n\x0f\x43ompanyResponse\x12\n\n\x02id\x18\x01 \x01(\x03\x12\x0c\n\x04name\x18\x02 \x01(\t\x12\x0c\n\x04\x64\x65sc\x18\x03 \x01(\t\x12\x0f\n\x07website\x18\x04 \x01(\t\x12\x0e\n\x06\x63onfig\x18\x05 \x01(\t\x12\x0c\n\x04tags\x18\x06 \x01(\t\x12\x0f\n\x07\x61\x64\x64ress\x18\x07 \x01(\t\x12\x0c\n\x04info\x18\x08 \x01(\t\x12\x12\n\ncreator_id\x18\t \x01(\x03\x12\x11\n\tparent_id\x18\n \x01(\x03\x12\x0e\n\x06status\x18\x0b \x01(\x05\"\xb7\x01\n\x14\x43reateCompanyRequest\x12\x0c\n\x04name\x18\x01 \x01(\t\x12\x0c\n\x04\x64\x65sc\x18\x02 \x01(\t\x12\x0f\n\x07website\x18\x03 \x01(\t\x12\x0e\n\x06\x63onfig\x18\x04 \x01(\t\x12\x0c\n\x04tags\x18\x05 \x01(\t\x12\x0f\n\x07\x61\x64\x64ress\x18\x06 \x01(\t\x12\x0c\n\x04info\x18\x07 \x01(\t\x12\x12\n\ncreator_id\x18\x08 \x01(\x03\x12\x11\n\tparent_id\x18\t \x01(\x03\x12\x0e\n\x06status\x18\n \x01(\x05\x32\xc3\x02\n\x07\x43ompany\x12>\n\x0eGetCompanyList\x12\x16.GetCompanyListRequest\x1a\x14.CompanyListResponse\x12>\n\x10GetCompanyDetail\x12\x18.GetCompanyDetailRequest\x1a\x10.CompanyResponse\x12\x38\n\rCreateCompany\x12\x15.CreateCompanyRequest\x1a\x10.CompanyResponse\x12>\n\rUpdateCompany\x12\x15.UpdateCompanyRequest\x1a\x16.google.protobuf.Empty\x12>\n\rDeleteCompany\x12\x15.DeleteCompanyRequest\x1a\x16.google.protobuf.Emptyb\x06proto3')
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\rcompany.proto\x1a\x1bgoogle/protobuf/empty.proto\"*\n\x17GetMyCompanyListRequest\x12\x0f\n\x07user_id\x18\x01 \x01(\x03\"-\n\x1bGetCompanyListByUserRequest\x12\x0e\n\x06userId\x18\x01 \x01(\x03\"%\n\x17GetCompanyDetailRequest\x12\n\n\x02id\x18\x01 \x01(\x03\"R\n\x15GetCompanyListRequest\x12\x0c\n\x04page\x18\x01 \x01(\x05\x12\r\n\x05limit\x18\x02 \x01(\x05\x12\x0e\n\x06search\x18\x03 \x01(\t\x12\x0c\n\x04sort\x18\x04 \x01(\t\"D\n\x13\x43ompanyListResponse\x12\x1e\n\x04\x64\x61ta\x18\x01 \x03(\x0b\x32\x10.CompanyResponse\x12\r\n\x05total\x18\x02 \x01(\x05\"\"\n\x14\x44\x65leteCompanyRequest\x12\n\n\x02id\x18\x01 \x01(\x03\"\xc3\x01\n\x14UpdateCompanyRequest\x12\n\n\x02id\x18\x01 \x01(\x03\x12\x0c\n\x04name\x18\x02 \x01(\t\x12\x0c\n\x04\x64\x65sc\x18\x03 \x01(\t\x12\x0f\n\x07website\x18\x04 \x01(\t\x12\x0e\n\x06\x63onfig\x18\x05 \x01(\t\x12\x0c\n\x04tags\x18\x06 \x01(\t\x12\x0f\n\x07\x61\x64\x64ress\x18\x07 \x01(\t\x12\x0c\n\x04info\x18\x08 \x01(\t\x12\x12\n\ncreator_id\x18\t \x01(\x03\x12\x11\n\tparent_id\x18\n \x01(\x03\x12\x0e\n\x06status\x18\x0b \x01(\x05\"\xbe\x01\n\x0f\x43ompanyResponse\x12\n\n\x02id\x18\x01 \x01(\x03\x12\x0c\n\x04name\x18\x02 \x01(\t\x12\x0c\n\x04\x64\x65sc\x18\x03 \x01(\t\x12\x0f\n\x07website\x18\x04 \x01(\t\x12\x0e\n\x06\x63onfig\x18\x05 \x01(\t\x12\x0c\n\x04tags\x18\x06 \x01(\t\x12\x0f\n\x07\x61\x64\x64ress\x18\x07 \x01(\t\x12\x0c\n\x04info\x18\x08 \x01(\t\x12\x12\n\ncreator_id\x18\t \x01(\x03\x12\x11\n\tparent_id\x18\n \x01(\x03\x12\x0e\n\x06status\x18\x0b \x01(\x05\"\xb7\x01\n\x14\x43reateCompanyRequest\x12\x0c\n\x04name\x18\x01 \x01(\t\x12\x0c\n\x04\x64\x65sc\x18\x02 \x01(\t\x12\x0f\n\x07website\x18\x03 \x01(\t\x12\x0e\n\x06\x63onfig\x18\x04 \x01(\t\x12\x0c\n\x04tags\x18\x05 \x01(\t\x12\x0f\n\x07\x61\x64\x64ress\x18\x06 \x01(\t\x12\x0c\n\x04info\x18\x07 \x01(\t\x12\x12\n\ncreator_id\x18\x08 \x01(\x03\x12\x11\n\tparent_id\x18\t \x01(\x03\x12\x0e\n\x06status\x18\n \x01(\x05\x32\xd3\x03\n\x07\x43ompany\x12>\n\x0eGetCompanyList\x12\x16.GetCompanyListRequest\x1a\x14.CompanyListResponse\x12>\n\x10GetCompanyDetail\x12\x18.GetCompanyDetailRequest\x1a\x10.CompanyResponse\x12\x38\n\rCreateCompany\x12\x15.CreateCompanyRequest\x1a\x10.CompanyResponse\x12>\n\rUpdateCompany\x12\x15.UpdateCompanyRequest\x1a\x16.google.protobuf.Empty\x12>\n\rDeleteCompany\x12\x15.DeleteCompanyRequest\x1a\x16.google.protobuf.Empty\x12J\n\x14GetCompanyListByUser\x12\x1c.GetCompanyListByUserRequest\x1a\x14.CompanyListResponse\x12\x42\n\x10GetMyCompanyList\x12\x18.GetMyCompanyListRequest\x1a\x14.CompanyListResponseB\nZ\x08../protob\x06proto3')
 
 
 
+_GETMYCOMPANYLISTREQUEST = DESCRIPTOR.message_types_by_name['GetMyCompanyListRequest']
+_GETCOMPANYLISTBYUSERREQUEST = DESCRIPTOR.message_types_by_name['GetCompanyListByUserRequest']
 _GETCOMPANYDETAILREQUEST = DESCRIPTOR.message_types_by_name['GetCompanyDetailRequest']
 _GETCOMPANYLISTREQUEST = DESCRIPTOR.message_types_by_name['GetCompanyListRequest']
 _COMPANYLISTRESPONSE = DESCRIPTOR.message_types_by_name['CompanyListResponse']
@@ -26,6 +28,20 @@ _DELETECOMPANYREQUEST = DESCRIPTOR.message_types_by_name['DeleteCompanyRequest']
 _UPDATECOMPANYREQUEST = DESCRIPTOR.message_types_by_name['UpdateCompanyRequest']
 _COMPANYRESPONSE = DESCRIPTOR.message_types_by_name['CompanyResponse']
 _CREATECOMPANYREQUEST = DESCRIPTOR.message_types_by_name['CreateCompanyRequest']
+GetMyCompanyListRequest = _reflection.GeneratedProtocolMessageType('GetMyCompanyListRequest', (_message.Message,), {
+  'DESCRIPTOR' : _GETMYCOMPANYLISTREQUEST,
+  '__module__' : 'company_pb2'
+  # @@protoc_insertion_point(class_scope:GetMyCompanyListRequest)
+  })
+_sym_db.RegisterMessage(GetMyCompanyListRequest)
+
+GetCompanyListByUserRequest = _reflection.GeneratedProtocolMessageType('GetCompanyListByUserRequest', (_message.Message,), {
+  'DESCRIPTOR' : _GETCOMPANYLISTBYUSERREQUEST,
+  '__module__' : 'company_pb2'
+  # @@protoc_insertion_point(class_scope:GetCompanyListByUserRequest)
+  })
+_sym_db.RegisterMessage(GetCompanyListByUserRequest)
+
 GetCompanyDetailRequest = _reflection.GeneratedProtocolMessageType('GetCompanyDetailRequest', (_message.Message,), {
   'DESCRIPTOR' : _GETCOMPANYDETAILREQUEST,
   '__module__' : 'company_pb2'
@@ -79,20 +95,25 @@ _COMPANY = DESCRIPTOR.services_by_name['Company']
 if _descriptor._USE_C_DESCRIPTORS == False:
 
   DESCRIPTOR._options = None
-  _GETCOMPANYDETAILREQUEST._serialized_start=46
-  _GETCOMPANYDETAILREQUEST._serialized_end=83
-  _GETCOMPANYLISTREQUEST._serialized_start=85
-  _GETCOMPANYLISTREQUEST._serialized_end=153
-  _COMPANYLISTRESPONSE._serialized_start=155
-  _COMPANYLISTRESPONSE._serialized_end=223
-  _DELETECOMPANYREQUEST._serialized_start=225
-  _DELETECOMPANYREQUEST._serialized_end=259
-  _UPDATECOMPANYREQUEST._serialized_start=262
-  _UPDATECOMPANYREQUEST._serialized_end=457
-  _COMPANYRESPONSE._serialized_start=460
-  _COMPANYRESPONSE._serialized_end=650
-  _CREATECOMPANYREQUEST._serialized_start=653
-  _CREATECOMPANYREQUEST._serialized_end=836
-  _COMPANY._serialized_start=839
-  _COMPANY._serialized_end=1162
+  DESCRIPTOR._serialized_options = b'Z\010../proto'
+  _GETMYCOMPANYLISTREQUEST._serialized_start=46
+  _GETMYCOMPANYLISTREQUEST._serialized_end=88
+  _GETCOMPANYLISTBYUSERREQUEST._serialized_start=90
+  _GETCOMPANYLISTBYUSERREQUEST._serialized_end=135
+  _GETCOMPANYDETAILREQUEST._serialized_start=137
+  _GETCOMPANYDETAILREQUEST._serialized_end=174
+  _GETCOMPANYLISTREQUEST._serialized_start=176
+  _GETCOMPANYLISTREQUEST._serialized_end=258
+  _COMPANYLISTRESPONSE._serialized_start=260
+  _COMPANYLISTRESPONSE._serialized_end=328
+  _DELETECOMPANYREQUEST._serialized_start=330
+  _DELETECOMPANYREQUEST._serialized_end=364
+  _UPDATECOMPANYREQUEST._serialized_start=367
+  _UPDATECOMPANYREQUEST._serialized_end=562
+  _COMPANYRESPONSE._serialized_start=565
+  _COMPANYRESPONSE._serialized_end=755
+  _CREATECOMPANYREQUEST._serialized_start=758
+  _CREATECOMPANYREQUEST._serialized_end=941
+  _COMPANY._serialized_start=944
+  _COMPANY._serialized_end=1411
 # @@protoc_insertion_point(module_scope)
--- company_srv/proto/company_pb2_grpc.py
+++ company_srv/proto/company_pb2_grpc.py
@@ -2,7 +2,7 @@
 """Client and server classes corresponding to protobuf-defined services."""
 import grpc
 
-from . import company_pb2 as company__pb2
+import company_pb2 as company__pb2
 from google.protobuf import empty_pb2 as google_dot_protobuf_dot_empty__pb2
 
 
@@ -40,6 +40,16 @@ class CompanyStub(object):
                 request_serializer=company__pb2.DeleteCompanyRequest.SerializeToString,
                 response_deserializer=google_dot_protobuf_dot_empty__pb2.Empty.FromString,
                 )
+        self.GetCompanyListByUser = channel.unary_unary(
+                '/Company/GetCompanyListByUser',
+                request_serializer=company__pb2.GetCompanyListByUserRequest.SerializeToString,
+                response_deserializer=company__pb2.CompanyListResponse.FromString,
+                )
+        self.GetMyCompanyList = channel.unary_unary(
+                '/Company/GetMyCompanyList',
+                request_serializer=company__pb2.GetMyCompanyListRequest.SerializeToString,
+                response_deserializer=company__pb2.CompanyListResponse.FromString,
+                )
 
 
 class CompanyServicer(object):
@@ -75,6 +85,18 @@ class CompanyServicer(object):
         context.set_details('Method not implemented!')
         raise NotImplementedError('Method not implemented!')
 
+    def GetCompanyListByUser(self, request, context):
+        """Missing associated documentation comment in .proto file."""
+        context.set_code(grpc.StatusCode.UNIMPLEMENTED)
+        context.set_details('Method not implemented!')
+        raise NotImplementedError('Method not implemented!')
+
+    def GetMyCompanyList(self, request, context):
+        """Missing associated documentation comment in .proto file."""
+        context.set_code(grpc.StatusCode.UNIMPLEMENTED)
+        context.set_details('Method not implemented!')
+        raise NotImplementedError('Method not implemented!')
+
 
 def add_CompanyServicer_to_server(servicer, server):
     rpc_method_handlers = {
@@ -102,6 +124,16 @@ def add_CompanyServicer_to_server(servicer, server):
                     servicer.DeleteCompany,
                     request_deserializer=company__pb2.DeleteCompanyRequest.FromString,
                     response_serializer=google_dot_protobuf_dot_empty__pb2.Empty.SerializeToString,
+            ),
+            'GetCompanyListByUser': grpc.unary_unary_rpc_method_handler(
+                    servicer.GetCompanyListByUser,
+                    request_deserializer=company__pb2.GetCompanyListByUserRequest.FromString,
+                    response_serializer=company__pb2.CompanyListResponse.SerializeToString,
+            ),
+            'GetMyCompanyList': grpc.unary_unary_rpc_method_handler(
+                    servicer.GetMyCompanyList,
+                    request_deserializer=company__pb2.GetMyCompanyListRequest.FromString,
+                    response_serializer=company__pb2.CompanyListResponse.SerializeToString,
             ),
     }
     generic_handler = grpc.method_handlers_generic_handler(
@@ -195,5 +227,39 @@ class Company(object):
         return grpc.experimental.unary_unary(request, target, '/Company/DeleteCompany',
             company__pb2.DeleteCompanyRequest.SerializeToString,
             google_dot_protobuf_dot_empty__pb2.Empty.FromString,
+            options, channel_credentials,
+            insecure, call_credentials, compression, wait_for_ready, timeout, metadata)
+
+    @staticmethod
+    def GetCompanyListByUser(request,
+            target,
+            options=(),
+            channel_credentials=None,
+            call_credentials=None,
+            insecure=False,
+            compression=None,
+            wait_for_ready=None,
+            timeout=None,
+            metadata=None):
+        return grpc.experimental.unary_unary(request, target, '/Company/GetCompanyListByUser',
+            company__pb2.GetCompanyListByUserRequest.SerializeToString,
+            company__pb2.CompanyListResponse.FromString,
+            options, channel_credentials,
+            insecure, call_credentials, compression, wait_for_ready, timeout, metadata)
+
+    @staticmethod
+    def GetMyCompanyList(request,
+            target,
+            options=(),
+            channel_credentials=None,
+            call_credentials=None,
+            insecure=False,
+            compression=None,
+            wait_for_ready=None,
+            timeout=None,
+            metadata=None):
+        return grpc.experimental.unary_unary(request, target, '/Company/GetMyCompanyList',
+            company__pb2.GetMyCompanyListRequest.SerializeToString,
+            company__pb2.CompanyListResponse.FromString,
             options, channel_credentials,
             insecure, call_credentials, compression, wait_for_ready, timeout, metadata)
