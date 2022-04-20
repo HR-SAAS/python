41d3dc7da5d644c3f265c06b297ea68a948950eb
blank <1136589038@qq.com>
2022-04-20 23:20:35 +0800

    fix: 同步proto

--- message_srv/model/model.py
+++ message_srv/model/model.py
@@ -7,6 +7,7 @@ class BaseModel(BaseModel):
     class Meta:
         database = config.DB
 
+
 # 你投递的某个流程：最新状态: xxxxx 点击此处区查看
 class UserMessage(BaseModel, DeletedModel):
     user_id = IntegerField(verbose_name="关联的用户id")
@@ -14,13 +15,13 @@ class UserMessage(BaseModel, DeletedModel):
     source_type = CharField(verbose_name='来源模型: company/user/system')
     source_id = IntegerField(verbose_name='源id: ')
 
-    relation_id=IntegerField(verbose_name='关联资源')
-    relation_type=CharField(verbose_name='关联类型')
+    relation_id = IntegerField(verbose_name='关联资源')
+    relation_type = CharField(verbose_name='关联类型')
 
     type = CharField(verbose_name='消息类型')
     content = TextField(verbose_name='内容')
 
-    is_read = BooleanField(verbose_name='已读?',default=False)
+    is_read = BooleanField(verbose_name='已读?', default=False)
     status = EnumField(verbose_name='状态: 正常|删除|其他', default=1)
 
 
